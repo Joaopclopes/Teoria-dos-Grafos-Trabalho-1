f90052afd5587bad27970cf43e1391e469be5b41
Joao Pedro Costa Lopes <68126578+Joaopclopes@users.noreply.github.com>
2022-05-10 21:27:59 -0300

    Add files via upload

---
+++ Teoria-dos-Grafos-Trabalho-1/Grafo.h
@@ -0,0 +1,19 @@
+#ifndef GRAFO_H_
+#define GRAFO_H_
+
+using namespace std;
+
+class Grafo
+{
+    private:
+    int Ordem;
+
+    public:
+
+    Grafo();
+
+    /*funcoes sem retorno*/
+
+    /*funcoes com retorno*/
+};
+#endif
